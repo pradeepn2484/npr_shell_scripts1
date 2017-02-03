b0f7d2f4692562673658a5fefee6bb9fde6423f4
pradeepn2484 <pradeepn2484@gmail.com>
2017-02-03 10:43:16 -0500

    Please enter the commit message for your changes. Lines starting

--- prog_name_EX1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Athor : N.Pradeep Reddy
-# Copyright (c) nprtech.com
-# Script follows here :
-
-echo " what is your name ?"
-read NAME  # read is used for reading the keyboard input
-echo "Hello, $NAME"
