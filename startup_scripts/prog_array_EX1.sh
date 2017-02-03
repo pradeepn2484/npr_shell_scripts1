ab49b250ec159f1dc4090ca6b663056655f9342f
pradeepn2484 <pradeepn2484@gmail.com>
2017-02-03 11:29:08 -0500

    added simple array program

---
+++ startup_scripts/prog_array_EX1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Athor : N.Pradeep Reddy
+# Copyright (c) nprtech.com
+# Script follows here :
+
+NAME[0]="Zara"    # assigning value array 0th location i.e starting
+NAME[1]="Qadir"   # assigning the valuse to 1st location i.e 2nd
+NAME[2]="Mahnaz"
+NAME[3]="Ayan"
+NAME[4]="Daisy"
+echo "First Method is array[*] : ${NAME[*]}"  # accessing method1
+echo "Second Methodis array[@] : ${NAME[@]}"  # accessing method2
+
