d5b4c9e55fb8458d719c700ef3f25f1ebd3168d2
root <root@ip-172-31-4-69.ec2.internal>
2017-02-03 10:33:19 -0500

    first testing program

---
+++ prog_name_EX1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Athor : N.Pradeep Reddy
+# Copyright (c) nprtech.com
+# Script follows here :
+
+echo " what is your name ?"
+read NAME  # read is used for reading the keyboard input
+echo "Hello, $NAME"
