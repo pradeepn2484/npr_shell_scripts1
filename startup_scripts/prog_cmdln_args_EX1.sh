aacf7733713ceec6d6c5702ca3814de42e9de3ec
pradeepn2484 <pradeepn2484@gmail.com>
2017-02-03 11:15:26 -0500

    added command line argument program1

---
+++ startup_scripts/prog_cmdln_args_EX1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Athor : N.Pradeep Reddy
+# Copyright (c) nprtech.com
+# Script follows here :
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+# Here is a sample run for the above script –
+# ./prog_cmdln_args_EX1.sh pradeep reddy
